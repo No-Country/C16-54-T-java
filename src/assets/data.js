a508e9d012a578e0c7642c9eefbea0cf45f7545f
Fpelourson <fede_pelourson@hotmail.com>
2024-02-15 15:29:32 -0300

    filtro-imagenes

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,17 +1,17 @@
 export const data = [
     {
-      id: 1, imgUrl: "src/assets/img/bg.jpg"
+      id: 1, imgUrl: "src/assets/img/1.jpg"
     },
     {
-      id: 2, imgUrl: "src/assets/img/ad.jpg"
+      id: 2, imgUrl: "src/assets/img/2.jpg"
     },
     {
-      id: 3, imgUrl: "src/assets/img/fff.jpg"
+      id: 3, imgUrl: "src/assets/img/1.jpg"
     },
     {
-      id: 4, imgUrl: "src/assets/img/de.jpg"
+      id: 4, imgUrl: "src/assets/img/2.jpg"
     },
     {
-      id: 5, imgUrl: "src/assets/img/ad.jpg"
+      id: 5, imgUrl: "src/assets/img/1.jpg"
     }
   ];
