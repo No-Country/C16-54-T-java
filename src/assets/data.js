11ac41317a448ab5e2de0fdda6b9a9c61d3af17d
Fpelourson <fede_pelourson@hotmail.com>
2024-02-15 17:54:36 -0300

    deploy

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,17 +1,17 @@
 export const data = [
     {
-      id: 1, imgUrl: "./public/cc.jpg"
+      id: 1, imgUrl: "/cc.jpg"
     },
     {
-      id: 2, imgUrl: "src/assets/img/ccc.jpg"
+      id: 2, imgUrl: "/ccc.jpg"
     },
     {
-      id: 3, imgUrl: "src/assets/img/cc.jpg"
+      id: 3, imgUrl: "/cc.jpg"
     },
     {
-      id: 4, imgUrl: "src/assets/img/ccc.jpg"
+      id: 4, imgUrl: "/ccc.jpg"
     },
     {
-      id: 5, imgUrl: "src/assets/img/cc.jpg"
+      id: 5, imgUrl: "/cc.jpg"
     }
   ];
