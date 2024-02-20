1723f2a6c17258e514caaaab1b5468a3c7a45a92
sistekor17 <jamesserrato17@gmail.com>
2024-02-20 17:04:24 -0300

    crud entidad User sin pruebas

--- back/src/main/java/com/gamestopia/Gamestopia/Repository/UserRepository.java
+++ back/src/main/java/com/gamestopia/Gamestopia/Repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.gamestopia.Gamestopia.Repository;
 
 import com.gamestopia.Gamestopia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 }
