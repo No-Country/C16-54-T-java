ca3239502099c8cb25713d013553482f0128746f
DaniellaB9 <dbaellamedina@gmail.com>
2024-02-27 14:55:34 -0500

    Endpoints para secciones del navbar probado ok

--- back/src/main/java/com/gamestopia/Gamestopia/Repository/GameRepository.java
+++ back/src/main/java/com/gamestopia/Gamestopia/Repository/GameRepository.java
@@ -2,8 +2,18 @@ package com.gamestopia.Gamestopia.Repository;
 
 import com.gamestopia.Gamestopia.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository <Game, String> {
+
+    @Query("SELECT g FROM Game g WHERE g.name LIKE %?1% OR g.category LIKE %?1% "
+            + "OR g.description LIKE %?1% OR g.developerCompany LIKE %?1%")
+    List<Game> findBySearch(String value);
+
+    List<Game> findByCategory(String category);
+
 }
