d34031dc58e1bfe272ee76e067d227e0ad41e9bb
Fernando Hasperue <fernando.hasperue@gmail.com>
2024-03-05 18:44:22 -0300

    Cargar Nuevo Juego metodo POST

--- src/Components/NewGame.jsx
+++ src/Components/NewGame.jsx
@@ -2,21 +2,62 @@ import { Button } from "@chakra-ui/button";
 import { Link } from "react-router-dom";
 import "./styles.css";
 import { Input, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
 
 const NewGame = () => {
-  
+
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [precio, setPrecio] = useState(0);
+  const [clasificacion, setClasificacion] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [desarrollador, setDesarrollador] = useState("");
+
+  const handleSubmit = async (e) => {
+    console.log("presionado");
+
+    e.preventDefault();
+
+    const data = {
+      name: nombre,
+      description: descripcion,
+      price: precio,
+      active: true,
+      promotion: false,
+      clasification: "E",
+      category: categoria,
+      developerCompany: desarrollador,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/game/create",
+        data
+      );
+
+      if (response.status == 200) {
+        window.location.href = "/Admin";
+      } else {
+        // Manejar el caso de error, por ejemplo, mostrar un mensaje de error
+      }
+    } catch (error) {
+      console.error("Error al enviar solicitud:", error);
+    }
+  };
   return (
     <div className="new-game">
       <div className="title-game">
         <h2>NUEVO JUEGO</h2>
       </div>
-      <div className="datos-new-game">
+      <form method="POST" onSubmit={handleSubmit} className="datos-new-game">
         <div className="datos-primarios">
-          <div className="inputs">
+          <div>
             <Text color={"#9FEADD"}>Nombre</Text>
             <Input
-              id="apellido"
-              required
+               required
+               id="nombre"
+               value={nombre}
+               onChange={(e) => setNombre(e.target.value)}
               background={"#C2CEDE"}
               type="text"
               w={"100%"}
@@ -25,7 +66,9 @@ const NewGame = () => {
             />
             <Text color={"#9FEADD"}>Categoria</Text>
             <Input
-              id="apellido"
+              id="categoria"
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
               required
               background={"#C2CEDE"}
               type="text"
@@ -35,7 +78,33 @@ const NewGame = () => {
             />
             <Text color={"#9FEADD"}>Precio</Text>
             <Input
-              id="apellido"
+              id="precio"
+              value={precio}
+              onChange={(e) => setPrecio(e.target.value)}
+              required
+              background={"#C2CEDE"}
+              type="number"
+              w={"100%"}
+              h={"3rem"}
+              fontWeight={"bold"}
+            />
+           {/*  <Text color={"#9FEADD"}>Clasificacion</Text>
+            <Input
+              id="clasificacion"
+              value={clasificacion}
+              onChange={(e) => setClasificacion(e.target.value)}
+              required
+              background={"#C2CEDE"}
+              type="text"
+              w={"100%"}
+              h={"3rem"}
+              fontWeight={"bold"}
+            /> */}
+            <Text color={"#9FEADD"}>Compañia desarrolladora</Text>
+            <Input
+              id="desarrollador"
+              value={desarrollador}
+              onChange={(e) => setDesarrollador(e.target.value)}
               required
               background={"#C2CEDE"}
               type="text"
@@ -48,7 +117,7 @@ const NewGame = () => {
             <Text color={"#9FEADD"}>Agregar Imagen</Text>
             <Input
               id="apellido"
-              required
+              
               background={"#C2CEDE"}
               type="text"
               w={"100%"}
@@ -58,7 +127,7 @@ const NewGame = () => {
             <Text color={"#9FEADD"}>Agregar Video</Text>
             <Input
               id="apellido"
-              required
+              
               background={"#C2CEDE"}
               type="text"
               w={"100%"}
@@ -70,7 +139,9 @@ const NewGame = () => {
         <div className="datos-secundarios">
           <Text color={"#9FEADD"}>Descripción</Text>
           <Input
-            id="apellido"
+            id="descripcion"
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
             required
             background={"#C2CEDE"}
             type="text"
@@ -79,55 +150,19 @@ const NewGame = () => {
             fontWeight={"bold"}
             marginTop={"1rem"}
           />
-          <Text color={"#9FEADD"} marginTop={"2rem"}>
-            Requisitos del sistema
-          </Text>
-
-          <div className="requisitos">
-            <div className="minimos">
-              <Text color={"#9FEADD"}>Mínimos</Text>
-              <Input
-                id="apellido"
-                required
-                background={"#C2CEDE"}
-                type="text"
-                w={"100%"}
-                h={"100%"}
-                fontWeight={"bold"}
-                marginTop={"1rem"}
-              />
-            </div>
-            <div className="recomendados">
-              <Text color={"#9FEADD"}>Recomendados</Text>
-              <Input
-                id="apellido"
-                required
-                background={"#C2CEDE"}
-                type="text"
-                w={"100%"}
-                h={"100%"}
-                fontWeight={"bold"}
-                marginTop={"1rem"}
-              />
-            </div>
-            
-          </div>
-          <div className="agregar">
-          <Link to={""}>
-            <Button
-              variant="solid"
-              color={"#0D1A2C"}
-              bg={"#879DBB"}
-              _hover={{ bg: "#9FEADD" }}
-            >
-              AGREGAR JUEGO
-            </Button>
-          </Link>
         </div>
+        <div className="agregar">
+          <Button
+          type="submit"
+            variant="solid"
+            color={"#0D1A2C"}
+            bg={"#879DBB"}
+            _hover={{ bg: "#9FEADD" }}
+          >
+            AGREGAR JUEGO
+          </Button>
         </div>
-        
-      </div>
-      
+        </form>
     </div>
   );
 };
